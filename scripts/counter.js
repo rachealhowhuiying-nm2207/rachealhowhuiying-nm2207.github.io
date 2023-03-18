ddb0276f0bf262b425f14ea00a855562b855b473
Racheal_How_Hui_Ying-nm2207 <E0970574@u.nus.edu>
2023-03-18 23:27:11 +0800

    Created skeleton for Final Project
    Created Navbar and outline for data story

--- scripts/counter.js
+++ scripts/counter.js
@@ -1,15 +1,7 @@
-let counter = 0;
+function showPokeball() {
+  window.alert("When this button is clicked, a for loop will be looping several images to show pokeball is moving.");
+}
 
-document.addEventListener("DOMContentLoaded", function () {
-  document.querySelector("button").onclick = count;
-});
-
-function count() {
-  counter++;
-
-  if (counter % 10 === 0) {
-    alert("Mouse would like to thank you for the food 🥙");
-  }
-
-  document.querySelector(".counter").innerHTML = counter;
+function showMap() {
+  window.alert("When certain part of a specific region in the map is clicked, website will bring user to another page to show image of the most popular pokemon in that region with a himbo stat chart to show its stats");
 }
