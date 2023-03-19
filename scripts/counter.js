db21d41232974d06c87e8e4fee6f5b52f8a96563
Racheal_How_Hui_Ying-nm2207 <E0970574@u.nus.edu>
2023-03-20 00:16:37 +0800

    Tidying up for week 9 draft
    - added link for weekly documentation on the homepage
    - added more comments
    - add a box in index.html for text
    - re-adjust some formatting

--- scripts/counter.js
+++ scripts/counter.js
@@ -1,7 +1,7 @@
 function showPokeball() {
-  window.alert("When this button is clicked, a for loop will be looping several images to show pokeball is moving.");
+  window.alert("When this button is clicked, a for loop will be looping several images to show the pokeball moving - this will act as an animation. The last image will show Tegepi welcoming the user.");
 }
 
 function showMap() {
-  window.alert("When certain part of a specific region in the map is clicked, website will bring user to another page to show image of the most popular pokemon in that region with a himbo stat chart to show its stats");
+  window.alert(" This image of all regions will be cropped into individual image of each region. Each region will then be taking a modal box. When a specific region in the map is clicked, the user will be shown the content in the respective modal box. The image of the most popular pokemon in that region with a radar chart to show its stats can be seen from the modal box. At the same time, the theme song for the clicked region will be played on click.");
 }
