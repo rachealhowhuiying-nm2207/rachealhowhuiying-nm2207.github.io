68df6cdc066fbe379b1f24d06a6fde824cb6ccd8
Racheal_How_Hui_Ying-nm2207 <E0970574@u.nus.edu>
2023-04-09 01:09:54 +0800

    Added a trial chart into 1st modal box

--- scripts/counter.js
+++ scripts/counter.js
@@ -2,7 +2,7 @@
 //When button below pokeball is clicked, function startLoop() will be looping several images to show the pokeball moving - this will act as an animation. The last image will show Togepi welcoming the user.
 
 //set array of images
-const imageUrls = ['pokeball_center.png', 'pokeball_tilt_left.png', 'pokeball_tilt_right.png', 'pokeball_semi_open.png', 'togepi_appearing.png', 'togepi_welcome.png','togepi_welcome.png'];
+const imageUrls = ['pokeball_semi_open.png', 'togepi_appearing.png', 'togepi_welcome.png','togepi_welcome.png'];
 let currentIndex = 0;
 const image = document.getElementById('image');
 let intervalId;
@@ -20,6 +20,8 @@ function startLoop() {
 }
 
 window.onload = startLoop();
+
+//make a modal box for each region image
 
 // Get the button elements
 const modal1Btn = document.getElementById('modal1-btn');
@@ -49,32 +51,6 @@ closeBtns.forEach((btn) => {
     modal2.style.display = 'none';
   });
 });
-// var modal = document.getElementById("modal1");
-// var modal2 = document.getElementById("modal2");
-// // Get the button that opens the modal
-// var btn = document.getElementById("modal1-btn");
-// var btn2 = document.getElementById("modal2-btn");
-
-// // Get the <span> element that closes the modal
-// var span = document.getElementsByClassName("close")[0];
-// // var span2 = document.getElementsByClassName("close")[0];
-
-// // When the user clicks the button, open the modal 
-// btn.onclick = function() {
-//   modal.style.display = "block";
-// }
-
-// btn2.onclick = function() {
-//   modal2.style.display = "block";
-// }
-
-// // When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-//   modal.style.display = "none";
-// }
-// // span2.onclick = function() {
-// //   modal2.style.display = "none";
-// // }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
@@ -84,8 +60,45 @@ window.onclick = function(event) {
   }
 }
 
-// window.onclick = function(event) {
-//   if (event.target == modal2) {
-//     modal2.style.display = "none";
-//   }
-// }
+//Use chartjs to make a radar chart for each pokemon, 
+//referred from: https://www.chartjs.org/docs/latest/charts/radar.html and week 9 class work
+
+//kanto: charizard
+var charizardChart = document.getElementById('kantoRadarChart').getContext('2d');
+const charizardData = {
+  labels: ['HP','Attack','Defence','Sp. Atk','Sp. Def','Speed'],
+  datasets: [{
+    label: 'Charizard',
+    data: [78,84,78,109,85,100],
+    fill: true,
+    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+    borderColor: 'rgb(255, 99, 132)',
+    pointBackgroundColor: 'rgb(255, 99, 132)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgb(255, 99, 132)'
+  }]
+};
+new Chart(charizardChart,
+{
+    type: "radar",
+    data: charizardData,
+    options: { 
+      scale: {
+        ticks: {
+          beginAtZero: true,
+          max: 110}
+        },
+        maintainAspectRatio: false,
+        legend: {
+            display: false,
+        },
+        title: {
+            display: true,
+            text:"Charizard's Base Stats",
+            fontFamily: "TrebuchetMS",
+            fontSize: 24,
+            fontColor: 'rgb(100,20,0)',
+        }
+    }
+});
