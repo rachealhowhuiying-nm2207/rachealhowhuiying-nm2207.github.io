84587d5f5c4904b07aa46525117687c628b4130c
Racheal_How_Hui_Ying-nm2207 <E0970574@u.nus.edu>
2023-04-12 23:50:59 +0800

    added gif to all modal boxes

--- scripts/counter.js
+++ scripts/counter.js
@@ -26,21 +26,51 @@ window.onload = startLoop();
 // Get the button elements
 const modal1Btn = document.getElementById('modal1-btn');
 const modal2Btn = document.getElementById('modal2-btn');
+const modal3Btn = document.getElementById('modal3-btn');
+const modal4Btn = document.getElementById('modal4-btn');
+const modal5Btn = document.getElementById('modal5-btn');
+const modal6Btn = document.getElementById('modal6-btn');
+const modal7Btn = document.getElementById('modal7-btn');
+const modal8Btn = document.getElementById('modal8-btn');
 
 // Get the modal elements
 const modal1 = document.getElementById('modal1');
 const modal2 = document.getElementById('modal2');
+const modal3 = document.getElementById('modal3');
+const modal4 = document.getElementById('modal4');
+const modal5 = document.getElementById('modal5');
+const modal6 = document.getElementById('modal6');
+const modal7 = document.getElementById('modal7');
+const modal8 = document.getElementById('modal8');
 
 // Get the close button elements
 const closeBtns = document.querySelectorAll('.close');
 
-// Add event listeners to the button elements
+// Add event listeners to all button elements. 
+//"Block" prevents HTML elements from being displayed on the sides of the modal boxes.
 modal1Btn.addEventListener('click', () => {
   modal1.style.display = 'block';
 });
-
 modal2Btn.addEventListener('click', () => {
   modal2.style.display = 'block';
+});
+modal3Btn.addEventListener('click', () => {
+  modal3.style.display = 'block';
+});
+modal4Btn.addEventListener('click', () => {
+  modal4.style.display = 'block';
+});
+modal5Btn.addEventListener('click', () => {
+  modal5.style.display = 'block';
+});
+modal6Btn.addEventListener('click', () => {
+  modal6.style.display = 'block';
+});
+modal7Btn.addEventListener('click', () => {
+  modal7.style.display = 'block';
+});
+modal8Btn.addEventListener('click', () => {
+  modal8.style.display = 'block';
 });
 
 // Add event listeners to the close button elements
@@ -49,14 +79,26 @@ closeBtns.forEach((btn) => {
     // Hide the modal when the close button is clicked
     modal1.style.display = 'none';
     modal2.style.display = 'none';
+    modal3.style.display = 'none';
+    modal4.style.display = 'none';
+    modal5.style.display = 'none';
+    modal6.style.display = 'none';
+    modal7.style.display = 'none';
+    modal8.style.display = 'none';
   });
 });
 
-// When the user clicks anywhere outside of the modal, close it
+// When the user clicks anywhere outside of the modal, the modal box can be closed
 window.onclick = function(event) {
-  if (event.target == modal1 || event.target == modal2 ) {
+  if (event.target == modal1 || event.target == modal2 || event.target == modal3 || event.target == modal4 || event.target == modal5 || event.target == modal6 || event.target == modal7 || event.target == modal8 ) {
     modal1.style.display = "none";
     modal2.style.display = "none";
+    modal3.style.display = 'none';
+    modal4.style.display = 'none';
+    modal5.style.display = 'none';
+    modal6.style.display = 'none';
+    modal7.style.display = 'none';
+    modal8.style.display = 'none';
   }
 }
 
@@ -87,7 +129,7 @@ new Chart(charizardChart,
       scale: {
         ticks: {
           beginAtZero: true,
-          max: 110}
+          max: 150}
         },
         maintainAspectRatio: false,
         legend: {
@@ -96,6 +138,286 @@ new Chart(charizardChart,
         title: {
             display: true,
             text:"Charizard's Base Stats",
+            fontFamily: "TrebuchetMS",
+            fontSize: 24,
+            fontColor: 'rgb(100,20,0)',
+        }
+    }
+});
+
+//johto: umbreon
+var umbreonChart = document.getElementById('johtoRadarChart').getContext('2d');
+const umbreonData = {
+  labels: ['HP','Attack','Defence','Sp. Atk','Sp. Def','Speed'],
+  datasets: [{
+    label: 'Umbreon',
+    data: [95,65,110,60,130,65],
+    fill: true,
+    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+    borderColor: 'rgb(255, 99, 132)',
+    pointBackgroundColor: 'rgb(255, 99, 132)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgb(255, 99, 132)'
+  }]
+};
+new Chart(umbreonChart,
+{
+    type: "radar",
+    data: umbreonData,
+    options: { 
+      scale: {
+        ticks: {
+          beginAtZero: true,
+          max: 150}
+        },
+        maintainAspectRatio: false,
+        legend: {
+            display: false,
+        },
+        title: {
+            display: true,
+            text:"Umbreon's Base Stats",
+            fontFamily: "TrebuchetMS",
+            fontSize: 24,
+            fontColor: 'rgb(100,20,0)',
+        }
+    }
+});
+
+//hoenn: rayquaza
+var rayquazaChart = document.getElementById('hoennRadarChart').getContext('2d');
+const rayquazaData = {
+  labels: ['HP','Attack','Defence','Sp. Atk','Sp. Def','Speed'],
+  datasets: [{
+    label: 'Rayquaza',
+    data: [105,150,90,150,90,95],
+    fill: true,
+    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+    borderColor: 'rgb(255, 99, 132)',
+    pointBackgroundColor: 'rgb(255, 99, 132)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgb(255, 99, 132)'
+  }]
+};
+new Chart(rayquazaChart,
+{
+    type: "radar",
+    data: rayquazaData,
+    options: { 
+      scale: {
+        ticks: {
+          beginAtZero: true,
+          max: 150}
+        },
+        maintainAspectRatio: false,
+        legend: {
+            display: false,
+        },
+        title: {
+            display: true,
+            text:"Rayquaza's Base Stats",
+            fontFamily: "TrebuchetMS",
+            fontSize: 24,
+            fontColor: 'rgb(100,20,0)',
+        }
+    }
+});
+
+//sinnoh: lucario
+var lucarioChart = document.getElementById('sinnohRadarChart').getContext('2d');
+const lucarioData = {
+  labels: ['HP','Attack','Defence','Sp. Atk','Sp. Def','Speed'],
+  datasets: [{
+    label: 'Lucario',
+    data: [70,110,70,115,70,90],
+    fill: true,
+    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+    borderColor: 'rgb(255, 99, 132)',
+    pointBackgroundColor: 'rgb(255, 99, 132)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgb(255, 99, 132)'
+  }]
+};
+new Chart(lucarioChart,
+{
+    type: "radar",
+    data: lucarioData,
+    options: { 
+      scale: {
+        ticks: {
+          beginAtZero: true,
+          max: 150}
+        },
+        maintainAspectRatio: false,
+        legend: {
+            display: false,
+        },
+        title: {
+            display: true,
+            text:"Lucario's Base Stats",
+            fontFamily: "TrebuchetMS",
+            fontSize: 24,
+            fontColor: 'rgb(100,20,0)',
+        }
+    }
+});
+
+//unova: chandelure
+var chandelureChart = document.getElementById('unovaRadarChart').getContext('2d');
+const chandelureData = {
+  labels: ['HP','Attack','Defence','Sp. Atk','Sp. Def','Speed'],
+  datasets: [{
+    label: 'Chandelure',
+    data: [60,55,90,145,90,80],
+    fill: true,
+    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+    borderColor: 'rgb(255, 99, 132)',
+    pointBackgroundColor: 'rgb(255, 99, 132)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgb(255, 99, 132)'
+  }]
+};
+new Chart(chandelureChart,
+{
+    type: "radar",
+    data: chandelureData,
+    options: { 
+      scale: {
+        ticks: {
+          beginAtZero: true,
+          max: 150}
+        },
+        maintainAspectRatio: false,
+        legend: {
+            display: false,
+        },
+        title: {
+            display: true,
+            text:"Chandelure's Base Stats",
+            fontFamily: "TrebuchetMS",
+            fontSize: 24,
+            fontColor: 'rgb(100,20,0)',
+        }
+    }
+});
+
+//kalos: greninja
+var greninjaChart = document.getElementById('kalosRadarChart').getContext('2d');
+const greninjaData = {
+  labels: ['HP','Attack','Defence','Sp. Atk','Sp. Def','Speed'],
+  datasets: [{
+    label: 'Greninja',
+    data: [72,95,67,103,71,122],
+    fill: true,
+    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+    borderColor: 'rgb(255, 99, 132)',
+    pointBackgroundColor: 'rgb(255, 99, 132)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgb(255, 99, 132)'
+  }]
+};
+new Chart(greninjaChart,
+{
+    type: "radar",
+    data: greninjaData,
+    options: { 
+      scale: {
+        ticks: {
+          beginAtZero: true,
+          max: 150}
+        },
+        maintainAspectRatio: false,
+        legend: {
+            display: false,
+        },
+        title: {
+            display: true,
+            text:"Greninja's Base Stats",
+            fontFamily: "TrebuchetMS",
+            fontSize: 24,
+            fontColor: 'rgb(100,20,0)',
+        }
+    }
+});
+
+//alola: mimikyu
+var mimikyuChart = document.getElementById('alolaRadarChart').getContext('2d');
+const mimikyuData = {
+  labels: ['HP','Attack','Defence','Sp. Atk','Sp. Def','Speed'],
+  datasets: [{
+    label: 'Mimikyu',
+    data: [55,90,80,50,105,96],
+    fill: true,
+    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+    borderColor: 'rgb(255, 99, 132)',
+    pointBackgroundColor: 'rgb(255, 99, 132)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgb(255, 99, 132)'
+  }]
+};
+new Chart(mimikyuChart,
+{
+    type: "radar",
+    data: mimikyuData,
+    options: { 
+      scale: {
+        ticks: {
+          beginAtZero: true,
+          max: 150}
+        },
+        maintainAspectRatio: false,
+        legend: {
+            display: false,
+        },
+        title: {
+            display: true,
+            text:"Mimikyu's Base Stats",
+            fontFamily: "TrebuchetMS",
+            fontSize: 24,
+            fontColor: 'rgb(100,20,0)',
+        }
+    }
+});
+
+//galar: dragapult
+var dragapultChart = document.getElementById('galarRadarChart').getContext('2d');
+const dragapultData = {
+  labels: ['HP','Attack','Defence','Sp. Atk','Sp. Def','Speed'],
+  datasets: [{
+    label: 'Dragapult',
+    data: [88,120,75,100,75,142],
+    fill: true,
+    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+    borderColor: 'rgb(255, 99, 132)',
+    pointBackgroundColor: 'rgb(255, 99, 132)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgb(255, 99, 132)'
+  }]
+};
+new Chart(dragapultChart,
+{
+    type: "radar",
+    data: dragapultData,
+    options: { 
+      scale: {
+        ticks: {
+          beginAtZero: true,
+          max: 150}
+        },
+        maintainAspectRatio: false,
+        legend: {
+            display: false,
+        },
+        title: {
+            display: true,
+            text:"Dragapult's Base Stats",
             fontFamily: "TrebuchetMS",
             fontSize: 24,
             fontColor: 'rgb(100,20,0)',
